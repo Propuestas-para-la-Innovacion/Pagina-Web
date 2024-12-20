89f80dc74eff4821d09017b5da7bb56b3d816e68
José Mariano Reyes <josemarianoreyesh02@gmail.com>
2024-12-20 13:15:45 -0600

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -5,11 +5,11 @@ module.exports = function (eleventyConfig) {
 
     eleventyConfig.setTemplateFormats("njk,html,md");
     
-    eleventyConfig.addPassthroughCopy('/src');
+    eleventyConfig.addPassthroughCopy('src');
     eleventyConfig.addPassthroughCopy('admin');
-    eleventyConfig.addPassthroughCopy('/css');
-    eleventyConfig.addPassthroughCopy('/js');
-    eleventyConfig.addPassthroughCopy('/images');
+    eleventyConfig.addPassthroughCopy('css');
+    eleventyConfig.addPassthroughCopy('js');
+    eleventyConfig.addPassthroughCopy('images');
 
     eleventyConfig.addNunjucksFilter("rmj", function(content) {
         return rmj(content);
