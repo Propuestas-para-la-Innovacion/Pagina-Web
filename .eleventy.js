734824691e8101c9beb9659f21f1294eba66c3ba
José Mariano Reyes <josemarianoreyesh02@gmail.com>
2024-12-20 12:32:26 -0600

    nuevos textos

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,15 @@
-const rmj = require('render-markdown-js')
+const rmj = require('render-markdown-js');
+const moment = require('moment'); 
 
 module.exports = function (eleventyConfig) {
 
     eleventyConfig.setTemplateFormats("njk,html,md");
     
-    eleventyConfig.addPassthroughCopy('src');
-    eleventyConfig.addPassthroughCopy('admin');
-    eleventyConfig.addPassthroughCopy('css');
-    eleventyConfig.addPassthroughCopy('js');
-    eleventyConfig.addPassthroughCopy('images');
+    eleventyConfig.addPassthroughCopy('/src');
+    eleventyConfig.addPassthroughCopy('/admin');
+    eleventyConfig.addPassthroughCopy('/css');
+    eleventyConfig.addPassthroughCopy('/js');
+    eleventyConfig.addPassthroughCopy('/images');
 
     eleventyConfig.addNunjucksFilter("rmj", function(content) {
         return rmj(content);
@@ -16,5 +17,9 @@ module.exports = function (eleventyConfig) {
 
     eleventyConfig.addNunjucksFilter("limit", function (array, limit) {
         return array.slice(0, limit);
+    });
+
+    eleventyConfig.addFilter("dateFormat", function(date, format) {
+        return moment(date).format(format);
     });
 }
